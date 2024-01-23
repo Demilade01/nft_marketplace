a2d789831a6484e41ee402d79fd708f734007f45
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-01-23 07:30:30 -0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1 class="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    )
+}
+
+export default Home;
