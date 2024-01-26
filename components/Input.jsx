b0c41b173b982af0df8a63782b840966f021a116
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-01-26 07:17:02 -0800

    implement nft context

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { NFTContext } from '../context/NFTContext';
 
 const Input = ({inputType, title, placeholder, handleClick}) => {
+  const { nftCurrency } = useContext(NFTContext);
+
   return (
     <div className='mt-10 w-full'>
       <p className='font-poppins dark:text-white text-nft-black-1 font-semibold text-xl'>{title}</p>
@@ -12,7 +15,7 @@ const Input = ({inputType, title, placeholder, handleClick}) => {
                placeholder={placeholder}
                onChange={handleClick}
             />
-            <p className='font-poppins dark:text-white text-nft-black-1 font-semibold text-xl'>ETH</p>
+            <p className='font-poppins dark:text-white text-nft-black-1 font-semibold text-xl'>{nftCurrency}</p>
         </div>
       ) : inputType === 'textarea' ? (
         <textarea
--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,23 @@
 import Script from 'next/script';
 import { ThemeProvider } from 'next-themes';
 
+import { NFTProvider } from '../context/NFTContext';
 import { Navbar, Footer } from "../components";
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => (
-  <ThemeProvider attribute="class">
-    <div className="dark:bg-nft-dark bg-white min-h-screen">
-        <Navbar />
-        <div className='pt-65 '>
-         <Component {...pageProps} />
-        </div>
-        <Footer />
-    </div>
-    <Script src="https://kit.fontawesome.com/9b2da8baae.js" crossorigin="anonymous"></Script>
-  </ThemeProvider>
+  <NFTProvider>
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+          <Navbar />
+          <div className='pt-65 '>
+          <Component {...pageProps} />
+          </div>
+          <Footer />
+      </div>
+      <Script src="https://kit.fontawesome.com/9b2da8baae.js" crossorigin="anonymous"></Script>
+    </ThemeProvider>
+  </NFTProvider>
 );
 
 export default MyApp;
